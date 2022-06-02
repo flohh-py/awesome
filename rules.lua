5f4700e0f04c18e448ebc1a892b98a8e14a9b772
flohh-py <jbmflohh@gmail.com>
2022-06-02 19:15:43 -0400

    general confign cahnge

--- rules.lua
+++ rules.lua
@@ -8,7 +8,7 @@ beautiful.init(gears.filesystem.get_configuration_dir() .. "mytheme.lua")
 local rules = {
   -- All clients will match this rule.
   {
-    rule = { },
+    rule = {},
     properties = {
       border_width = beautiful.border_width,
       border_color = beautiful.border_normal,
@@ -17,17 +17,17 @@ local rules = {
       keys = keys.clientkeys,
       buttons = keys.clientbuttons,
       screen = awful.screen.preferred,
-      placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+      placement = awful.placement.no_overlap + awful.placement.no_offscreen,
       size_hints_honor = false
     }
   },
   -- Floating clients.
-	{
+  {
     rule_any = {
-		  instance = {
-			  "DTA",  -- Firefox addon DownThemAll.
-			  "copyq",  -- Includes session name in class.
-			  "pinentry",
+      instance = {
+        "DTA", -- Firefox addon DownThemAll.
+        "copyq", -- Includes session name in class.
+        "pinentry",
       },
       class = {
         "Arandr",
@@ -37,12 +37,12 @@ local rules = {
       -- Note that the name property shown in xprop might be set slightly after creation of the client
       -- and the name shown there might not match defined rules here.
       name = {
-        "Event Tester",  -- xev.
+        "Event Tester", -- xev.
       },
       role = {
-        "AlarmWindow",  -- Thunderbird's calendar.
-        "ConfigManager",  -- Thunderbird's about:config.
-        "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+        "AlarmWindow", -- Thunderbird's calendar.
+        "ConfigManager", -- Thunderbird's about:config.
+        "pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
       }
     },
     properties = {
@@ -51,11 +51,11 @@ local rules = {
   },
 
   -- Add titlebars to normal clients and dialogs
-	{
+  {
     rule_any = {
-    type = {
-      "normal",
-      "dialog"
+      type = {
+        "normal",
+        "dialog"
       }
     },
     properties = {
